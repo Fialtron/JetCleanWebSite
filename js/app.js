05e4045b0629eb525c0d0833c4118161928278da
Fialtron <moreno.juandi73@gmail.com>
2020-11-01 03:50:20 -0500

    Actualizacion general de estilos  y de services

--- js/app.js
+++ js/app.js
@@ -94,28 +94,28 @@ app = (() => {
             <ul>
                 <li>
                     <figure>
-                      <img src="img/alfombra.jpg" alt="El nuevo concepto de limpieza">
+                      <img src="img/WhatsApp Image 2020-10-28 at 10.36.36 PM.jpeg" alt="El nuevo concepto de limpieza">
                     </figure>
                     <div>
-                      <h3>carpet cleannig</h3>
+                      <h3>Pisos waxing</h3>
                       <p>Descripcion del valor</p>
                     </div>
                 </li>
                 <li>
                     <div>
-                      <h3>Daily cleaning</h3>
+                      <h3>Daily desinfection</h3>
                       <p>Descripcion del valor</p>
                     </div>
                     <figure>
-                      <img src="img/pisos.jpg" alt="El nuevo concepto de limpieza">
+                      <img src="img/WhatsApp Image 2020-10-28 at 10.44.03 PM (3).jpeg" alt="El nuevo concepto de limpieza">
                     </figure>
                 </li>
                 <li>
                     <figure>
-                      <img src="img/ventanas.jpg" alt="El nuevo concepto de limpieza">
+                      <img src="img/WhatsApp Image 2020-10-28 at 10.40.37 PM.jpeg" alt="El nuevo concepto de limpieza">
                     </figure>
                     <div>
-                      <h3>Remove graffiti</h3>
+                      <h3>Remove snow</h3>
                       <p>Descripcion del valor</p>
                     </div>
                 </li>
@@ -155,6 +155,7 @@ app = (() => {
   };
 
   const insertServicesSection = () => {
+
     const content = $('#contenido');
     cleanHTMLTags(content);
     document.getElementById("currentTransitionStyle").setAttribute("href", "css/services.css");
@@ -163,14 +164,78 @@ app = (() => {
         <figure>
             <img src="img/Aspiradora.jpg" alt="El nuevo concepto de limpieza">
             <div>
-                <h1>Jet Clean</h1>
-                <h2>A new cleanning concept</h2>
-                <h3>We are experts about cleanning </h3>
+                <h2>Services</h2>
+                <h3>Close your eyes and breath</h3>
             </div>
         </figure>
     </header>
     <div id="servicesContainer">
+
+      <section id="menu">
+
+      <div id="scene">
+      <div id="left-zone">
+        <ul class="list">
+          <li class="item">
+            <input type="radio" id="radio_carpetCleaning" name="basic_carousel" checked="checked"/>
+            <label class="label_carpetCleaning" for="radio_carpetCleaning">Carpet Cleaning</label>
+            <div class="content ">
+              <div class="why">
+                <img src="img/carpet.jpeg" alt="">
+                <h2>¿Por que?</h2>
+                <p>The garden strawberry (or simply strawberry /ˈstrɔːbᵊri/; Fragaria × ananassa) is a widely grown hybrid species of the genus Fragaria (collectively known as the strawberries)</p>
+              </div>
+              <div >
+                <h2>Nuestro trabajo</h2>
+                <p>The garden strawberry (or simply strawberry /ˈstrɔːbᵊri/; Fragaria × ananassa) is a widely grown hybrid species of the genus Fragaria (collectively known as the strawberries)</p>
+              </div>
+              <div >
+                <h2>Herramientas que usamos</h2>
+                <p>The garden strawberry (or simply strawberry /ˈstrɔːbᵊri/; Fragaria × ananassa) is a widely grown hybrid species of the genus Fragaria (collectively known as the strawberries)</p>
+              </div>
+              
+              
+            </div>
+          </li>
+          <li class="item">
+            <input type="radio" id="radio_A banana is an edible fruit, botanically a berry, produced by several kinds of large herbaceous flowering plants in the genus Musa." name="basic_carousel" value="A banana is an edible fruit, botanically a berry, produced by several kinds of large herbaceous flowering plants in the genus Musa."/>
+            <label class="label_banana" for="radio_A banana is an edible fruit, botanically a berry, produced by several kinds of large herbaceous flowering plants in the genus Musa.">banana</label>
+            <div class="content content_banana"><span class="picto"></span>
+              <h1>banana</h1>
+              <p>A banana is an edible fruit, botanically a berry, produced by several kinds of large herbaceous flowering plants in the genus Musa.</p>
+            </div>
+          </li>
+          <li class="item">
+            <input type="radio" id="radio_The apple tree (Malus domestica) is a deciduous tree in the rose family best known for its sweet, pomaceous fruit, the apple. It is cultivated worldwide as a fruit tree, and is the most widely grown species in the genus Malus." name="basic_carousel" value="The apple tree (Malus domestica) is a deciduous tree in the rose family best known for its sweet, pomaceous fruit, the apple. It is cultivated worldwide as a fruit tree, and is the most widely grown species in the genus Malus."/>
+            <label class="label_apple" for="radio_The apple tree (Malus domestica) is a deciduous tree in the rose family best known for its sweet, pomaceous fruit, the apple. It is cultivated worldwide as a fruit tree, and is the most widely grown species in the genus Malus.">apple</label>
+            <div class="content content_apple"><span class="picto"></span>
+              <h1>apple</h1>
+              <p>The apple tree (Malus domestica) is a deciduous tree in the rose family best known for its sweet, pomaceous fruit, the apple. It is cultivated worldwide as a fruit tree, and is the most widely grown species in the genus Malus.</p>
+            </div>
+          </li>
+          <li class="item">
+            <input type="radio" id="radio_The orange (specifically, the sweet orange) is the fruit of the citrus species Citrus × sinensis in the family Rutaceae." name="basic_carousel" value="The orange (specifically, the sweet orange) is the fruit of the citrus species Citrus × sinensis in the family Rutaceae."/>
+            <label class="label_orange" for="radio_The orange (specifically, the sweet orange) is the fruit of the citrus species Citrus × sinensis in the family Rutaceae.">orange</label>
+            <div class="content content_orange"><span class="picto"></span>
+              <h1>orange</h1>
+              <p>The orange (specifically, the sweet orange) is the fruit of the citrus species Citrus × sinensis in the family Rutaceae.</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div id="middle-border"></div>
+      <div id="right-zone"></div>
+    </div>
+    </section>
+  
+
+
+
+
+
+
     <div id="slider">
+    <h2>Resultados</h2> 
         <div class="wrapper">
   <input checked type=radio name="slider" id="slide1" />
   <input type=radio name="slider" id="slide2" />
@@ -220,11 +285,11 @@ app = (() => {
   <!-- .slider-wrapper -->
 
   <div class="slider-prev-next-control">
-    <label for=slide1></label>
-    <label for=slide2></label>
-    <label for=slide3></label>
-    <label for=slide4></label>
-    <label for=slide5></label>
+    <label for=slide1><img src="img/flecha-izquierda.svg"/></label>
+    <label for=slide2><img src="img/proximo.svg"/></label>
+    <label for=slide3><img src="img/proximo.svg"/></label>
+    <label for=slide4><img src="img/flecha-izquierda.svg"/></label>
+    <label for=slide5><img src="img/flecha-izquierda.svg"/></label>
   </div>
   <!-- .slider-prev-next-control -->
 
@@ -239,6 +304,9 @@ app = (() => {
 </div>
     </div>
     `);
+  
+
+  
   };
 
   const insertAboutUsSection = () => {
